38bb93e75aacadc44c5e015d022fbfec5e5cca5b
Lucas Juarez <lucasmaximilianojuarez14@gmail.com>
2022-09-02 11:44:42 -0300

    creo carpeta m1u4, creo index.html,creo archivo js

---
+++ m1u4/js.js
@@ -0,0 +1,31 @@
+// CODIGO EJERCICIO 1
+
+let distancia = prompt ("Ingrese distancia a recorrer en metros.")
+
+
+
+if (distancia <= 1000 ) {
+    document.write("Le sugerimos viajar a pie");
+} else if (distancia > 1000 && distancia <= 10000 ) {
+    document.write("Le sugerimos viajar en bicicleta")
+} if(distancia > 10000 && distancia <= 30000) {
+    document.write("Le sugerimos viajar en transporte publico")
+} else if (distancia > 30000 && distancia <= 100000) {
+    document.write("Le sugerimos viajar en auto")
+} if (distancia > 100000) {
+    document.write("Le sugerimos viajar en avion")
+}
+
+
+
+//CODIGO EJERCICIO 2
+
+
+
+
+
+
+
+
+
+
